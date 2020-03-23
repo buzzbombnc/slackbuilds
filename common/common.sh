9ee2cdb269562ae4f50d540c8f344cba79c7f3d3
buzzbombnc <buzzbomb-github@buzzbomb.net>
2020-03-22 21:26:28 -0400

    Remove exit-on-error and add wget download.

--- common/common.sh
+++ common/common.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 #
 # Common SlackBuild functions
 
@@ -11,4 +11,14 @@ slackbuild_init() {
     PKG=$TMP/package-${NAME}
     rm -rf $PKG
     mkdir -p $PKG
+}
+
+# URL is in $1
+# Optional filename to save to is $2.
+slackbuild_download() {
+    local CMD="wget -N -c "
+    [[ -n "$2" ]] && CMD+="-o '$2' "
+    CMD+="'$1'"
+
+    eval $CMD
 }
