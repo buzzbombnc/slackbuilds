ae6f154e1d85181217bcafcc12bba5a1bc66b285
Ken Treadway <buzzbomb-github@buzzbomb.net>
2022-08-05 23:36:23 -0400

    Update docker_static with latest versions and runit support.

--- docker_static/doinst.sh
+++ docker_static/doinst.sh
@@ -1,8 +1,23 @@
-system_group docker
+if ! group_exists docker; then
+    DOCKER_GID=$(find_system_gid_uid gid)
+    echo "Creating 'docker' with gid $DOCKER_GID."
+    groupadd -g $DOCKER_GID docker
+fi
 
-config etc/default/docker.default.new
+config etc/docker/daemon.json.new
+config etc/default/docker.new
 config etc/rc.d/rc.docker.new
-configdel etc/logrotate.d/docker.new
+config etc/logrotate.d/docker.new
+
+if [[ ! -d etc/service/dockerd ]]; then
+    # If the runit service config doesn't already exist, move our disabled one over.
+    mv etc/service/.dockerd etc/service/dockerd
+else
+    # Otherwise, update all not-'down' files with the latest info and remove the dir.
+    find etc/service/.dockerd -name 'down' -delete
+    cp -a etc/service/.dockerd/* etc/service/dockerd/
+    rm -rf etc/service/.dockerd
+fi
 
 # Add Docker to rc.local, if it's not there.
 if [[ -f etc/rc.d/rc.local ]] && ! grep -q /etc/rc.d/rc.docker etc/rc.d/rc.local; then
@@ -16,3 +31,5 @@ echo ""
 echo "If you want Docker to run at startup:"
 echo ""
 echo "chmod +x /etc/rc.d/rc.docker"
+echo "             OR"
+echo "rm /etc/service/dockerd/{down,log/down}"
